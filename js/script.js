e83282a3a5b571a0ab2c1a1abb80970cfef41711
Haron Cavalaro Moreira <haron.moreira@surf.com.br>
2022-11-13 21:01:40 -0300

    baixa ai manolo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,51 @@
 function aviso () 
 {
-    alert("Um novo alerta!")
+    alert("Bem vindo, frangolino!")
+}
+
+function change() 
+{
+    let natacao = document.getElementById("natacao-img")
+    let musculacao = document.getElementById("musculacao-img")
+    let luta = document.getElementById("luta-img")
+    let danca = document.getElementById("danca-img")
+
+    let lista = [natacao, musculacao, luta, danca]
+
+    setInterval(function () { 
+        this.mudaStyle(lista)
+    } , 2000)
+
+}
+
+
+function mudaStyle(lista)
+{
+    setTimeout(function () { 
+        lista[0].setAttribute("style", "width: 10vw; height: 20vh;")
+        lista[1].removeAttribute("style")
+        lista[2].removeAttribute("style")
+        lista[3].removeAttribute("style")
+    } , 500)
+
+    setTimeout(function () { 
+        lista[1].setAttribute("style", "width: 10vw; height: 20vh;")
+        lista[0].removeAttribute("style")
+        lista[2].removeAttribute("style")
+        lista[3].removeAttribute("style")
+    } , 1000)
+
+    setTimeout(function () { 
+        lista[2].setAttribute("style", "width: 10vw; height: 20vh;")
+        lista[1].removeAttribute("style")
+        lista[0].removeAttribute("style")
+        lista[3].removeAttribute("style")
+    } , 1500)
+
+    setTimeout(function () { 
+        lista[3].setAttribute("style", "width: 10vw; height: 20vh;")
+        lista[1].removeAttribute("style")
+        lista[2].removeAttribute("style")
+        lista[0].removeAttribute("style")
+    } , 2000)
 }
